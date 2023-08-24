be1093b9f1f9e1b8f659a9ef4bd2d2b4b2a7912b
dani <danifromecuador@gmail.com>
2023-08-24 16:20:04 -0500

    Fix rubocop linter errors

--- enumerable.rb
+++ enumerable.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 module MyEnumerable
-    def all?
-      each { |item| return false unless yield(item) }
-      true
-    end
-  
-    def any?
-      each { |item| return true if yield(item) }
-      false
-    end
-  
-    def filter
-      result = []
-      each { |item| result << item if yield(item) }
-      result
-    end
+  def all?
+    each { |item| return false unless yield(item) }
+    true
+  end
+
+  def any?
+    each { |item| return true if yield(item) }
+    false
+  end
+
+  def filter
+    result = []
+    each { |item| result << item if yield(item) }
+    result
+  end
 end
